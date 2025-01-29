7db5a8191095e69e2132555dc068513d272c3c51
ankitingithub <as0617300@gmail.com>
2025-01-29 20:06:02 +0530

    Added Edit Post

--- src/Post.js
+++ src/Post.js
@@ -1,23 +1,23 @@
 import { formatISO9075 } from "date-fns";
+import { Link } from "react-router-dom";
 
-function Post({ title, summary, content, cover, createdAt,author }) {
+function Post({ _id, title, summary, content, cover, createdAt, author }) {
   return (
     <div className="post">
       <div className="image">
-        <img
-          src={'http://localhost:4000/'+cover}
-          alt=""
-        ></img>
+        <Link to={`/post/${_id}`}>
+          <img src={"http://localhost:4000/" + cover} alt=""></img>
+        </Link>
       </div>
       <div className="texts">
-        <h2>{title}</h2>
+        <Link to={`/post/${_id}`}>
+          <h2>{title}</h2>
+        </Link>
         <p className="info">
           <a className="author">{author.username}</a>
           <time>{formatISO9075(new Date(createdAt))}</time>
         </p>
-        <p className="summary">
-          {summary}
-        </p>
+        <p className="summary">{summary}</p>
       </div>
     </div>
   );
